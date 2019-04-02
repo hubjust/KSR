b7df7554c039f906b8eeacdab1031f9dd85ba732
Justyna Hubert <37309986+hubjust@users.noreply.github.com>
2019-04-02 19:24:18 +0200

    File reader and article added

---
+++ KSR/KSR/KSR/Article.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace KSR
+{
+    public class Article
+    {
+        public string Title { get; set; }
+        public Dictionary<string, List<string>> Tags { get; set; }
+        public List<string> Words { get; set; }
+    }
+}
---
+++ KSR/KSR/KSR/FileReader.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+using HtmlAgilityPack;
+
+namespace KSR
+{
+    public class FileReader
+    {
+        public IEnumerable<Article> ObtainVectorSpaceModels()
+        {
+            for (int i = 0; i < 7; i++)
+            {
+                var rawXml = File.ReadAllText($"../../Data/reut2-{i.ToString().PadLeft(3, '0')}.sgm");
+                var doc = new HtmlDocument();
+                doc.LoadHtml(rawXml);
+                var regex = new Regex("[^a-zA-Z]");
+
+                foreach (var article in doc.DocumentNode.Descendants("REUTERS"))
+                {
+                    var body = article.Descendants("BODY").FirstOrDefault();
+                    var tags = new Dictionary<string, List<string>>();
+                    foreach (var tagNode in article.ChildNodes.Where(node => node.ChildNodes.Any(htmlNode => htmlNode.Name == "d")))
+                    {
+                        tags[tagNode.Name] = tagNode.Descendants("D").Select(node => node.InnerText).ToList();
+                    }
+
+                    if (body != null && tags.Count > 0)
+                    {
+                        yield return new Article
+                        {
+                            Words = regex.Replace(body.InnerText, " ").ToLower().Split(' ').Where(s => s.Length > 2)
+                                .ToList(),
+                            Tags = tags,
+                            Title = article.Descendants("TITLE").First().InnerText
+                        };
+                    }
+                }
+            }
+        }
+    }
+}
