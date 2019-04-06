7541afdfb2241da631bf20453b3d0e1d52d79cb7
Justyna Hubert <37309986+hubjust@users.noreply.github.com>
2019-04-06 16:34:55 +0200

    knn algortihm, fixes in euclidean and article, test vector and training vectors

---
+++ Zadanie 1/Logic/KnnAlgorithm.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logic
+{
+    public class KnnAlgorithm
+    {
+        public static bool Calculate(TestVectorAndTrainingVectors testAndTrainingVectors, int k)
+        {
+            return ChooseKNeighbours(testAndTrainingVectors, k);
+        }
+
+        public static bool ChooseKNeighbours(TestVectorAndTrainingVectors TestAndTrainingPair, int k)
+        {
+            TestAndTrainingPair.TrainingVectors = TestAndTrainingPair.TrainingVectors.OrderBy(h => h.Distance).ToList(); //sortuje
+            var Kneighbours = TestAndTrainingPair.TrainingVectors.Take(k).ToList(); //bierze k sąsiadów
+
+            return CheckPlace(Kneighbours, TestAndTrainingPair.TestVector);
+        }
+
+        // sprawdza, czy w państwa z testowych danych znajdują się w podanych przez nas artykułach
+        public static bool CheckPlace(List<Article> neighbours, Article testArticle) 
+        {
+            Dictionary<string, int> howManyPlaces = new Dictionary<string, int>();
+            int howManyTimesOccur = 0;
+
+            for (int i = 0; i < neighbours.Count; i++)
+            {
+                for (int j = 0; j < neighbours.Count; j++)
+                {
+                    if (neighbours.ElementAt(i).Places.First() == neighbours.ElementAt(j).Places.First())
+                    {
+                        howManyTimesOccur++;
+                    }
+                }
+
+                if (howManyPlaces.ContainsKey(neighbours.ElementAt(i).Places.First())) //zeruje licznik żeby nie powtarzać juz raz dodanych państsw 
+                {
+                    howManyTimesOccur = 0;
+                    continue;
+                }
+
+                howManyPlaces.Add(neighbours.ElementAt(i).Places.First(), howManyTimesOccur);
+                howManyTimesOccur = 0;
+            }
+
+            howManyPlaces = howManyPlaces.OrderByDescending(x => x.Value)
+                .ToDictionary(pair => pair.Key, pair => pair.Value);
+
+            List<KeyValuePair<string, int>> result = howManyPlaces.ToList();
+            var FoundPlace = result.First().Key;
+
+            if (testArticle.Places.First().Equals(FoundPlace))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+    }
+}
---
+++ Zadanie 1/Logic/TestVectorAndTrainingVectors.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logic
+{
+    public class TestVectorAndTrainingVectors
+    {
+        public Article TestVector { get; set; }
+        public List<Article> TrainingVectors { get; set; }
+    }
+}
