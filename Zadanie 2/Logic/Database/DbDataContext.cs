8c1c188bdd38524d675074e7376eebb76bd04e2e
Justyna Hubert <37309986+hubjust@users.noreply.github.com>
2019-06-15 19:08:59 +0200

    ViewModel update

---
+++ Zadanie 2/Logic/Database/DbDataContext.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Linq;
+using System.Data.Linq.Mapping;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logic.Database
+{
+    public partial class DbDataContext : DataContext
+    {
+        public DbDataContext(string fileOrServerOrConnection) : base(fileOrServerOrConnection)
+        {
+        }
+
+        public System.Data.Linq.Table<FifaPlayer> FifaPlayer
+        {
+            get
+            {
+                return GetTable<FifaPlayer>();
+            }
+        }
+    }
+}
--- Zadanie 2/ViewModel/MainViewModel.cs
+++ Zadanie 2/ViewModel/MainViewModel.cs
@@ -1,10 +1,18 @@
-﻿using System.Collections.Generic;
-using System.Windows.Input;
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.IO;
 using System.Linq;
-using System.Windows.Forms;
-using System;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
 
 using Logic;
+using Logic.Database;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
 
 namespace ViewModel
 {
@@ -13,25 +21,55 @@ namespace ViewModel
         public ICommand QuitCommand { get; set; }
         public ICommand CreateMessagesCommand { get; set; }
         public ICommand CreateComplexMessagesCommand { get; set; }
+        public ICommand SaveCommand { get; set; }
+
+        private DbDataContext dataContext;
 
         private List<string> QualifierList;
         private List<string> FirstSummarizerList;
         private List<string> SecondSummarizerList;
 
+        public LinguisticVariable SelectedQualifier { get; set; }
+        public LinguisticVariable SelectedSummarizer1 { get; set; }
+        public LinguisticVariable SelectedSummarizer2 { get; set; }
+        private ObservableCollection<LinguisticVariable> quantifiers;
+        public ObservableCollection<LinguisticVariable> LinguisticVariables { get; set; }
+
+        public LinguisticVariable SelectedFunction { get; set; }
+
+        public event PropertyChangedEventHandler PropertyChanged;
 
         public bool ConjunctionAndRB { get; set; }
         public bool ConjunctionOrRB { get; set; }
 
+        public string Output
+        {
+            get => output;
+            private set
+            {
+                output = value;
+                OnPropertyChanged("Output");
+            }
+        }
+        private string output;
 
-        public MainViewModel()
+
+        public MainViewModel(DbDataContext dataContext)
         {
             ConjunctionAndRB = true;
+
+            this.dataContext = dataContext;
+            LinguisticVariables = Variable.getAllVariables();
+            quantifiers = Quantifier.getAllQuantifiers();
 
             CreateMessagesCommand = new RelayCommand(SimpleMessages);
             CreateComplexMessagesCommand = new RelayCommand(ComplexMessages);
             QuitCommand = new RelayCommand(Quit);
         }
 
+        public MainViewModel()
+        {
+        }
 
         private void SimpleMessages()
         {
@@ -45,7 +83,22 @@ namespace ViewModel
 
         private void CreateMessages(bool isComplex = false)
         {
+           
+        }
 
+        protected void OnPropertyChanged(string name)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+
+        private void Save()
+        {
+            string path = "savedData.txt";
+
+            if (!File.Exists(path))
+            {
+                File.WriteAllText(path, Output);
+            }
         }
 
         private void Quit()
