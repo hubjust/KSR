48858a9e838b37e1cff0873bcafe7a7a69a7dcc7
Karol Podlewski <podlewski@hotmail.com>
2019-06-15 16:14:19 +0200

    Merged classes

--- Zadanie 2/Logic/LinguisticVariable.cs
+++ Zadanie 2/Logic/LinguisticVariable.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 using Logic.Database;
 using Logic.MembershipFunctions;
 
@@ -11,16 +8,17 @@ namespace Logic
     public class LinguisticVariable
     {
         public string Name { get; set; }
-        public IMembershipFunction MembershipFunction { get; set; }
-        public string MemberToExtract { get; set; }
-        public Func<FifaPlayer, double> Extractor { get; set; }
         public bool Absolute { get; set; }
-        public FuzzySet FuzzySet { get; set; }
+        public string MemberToExtract { get; set; }
+        public IMembershipFunction MembershipFunction { get; set; }
+        public Func<FifaPlayer, double> Extractor { get; set; }
+        public Func<FifaPlayer, double> FieldExtractor { get; set; }
 
         public double GetMemebership(FifaPlayer player)
         {
-            return FuzzySet.GetMembership(player);
+            return MembershipFunction.GetMembership(Extractor(player));
         }
         public string MemberAndName { get => MemberToExtract + ": " + Name; }
+
     }
 }
